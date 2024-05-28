df5d10684ce30aaf39790b9a677370ff759c651c
nigradwohl <you@example.comnico.gradwohl@uni-konstanz.de>
2024-05-28 15:03:32 +0200

    create wiki object to contain overarching information

---
+++ www/scripts/build_wiki.js
@@ -0,0 +1,47 @@
+const wiki_order = ["prozent", "nh"]
+
+
+$(document).ready(function () {
+
+    let wiki_text = "";
+
+    for (const entry of wiki_order) {
+
+        const curinfo = info_data[entry];
+
+        wiki_text += "<div class=\"wiki-entry full-column\" id=\"wiki-" + entry + "\">";
+
+        wiki_text += '<h2>' + curinfo.heading + '</h2>';
+
+        if(curinfo.annotation.length > 0){
+            wiki_text += curinfo.annotation;
+        }
+
+        // Overview information from object:
+        if (curinfo.overview.length > 0) {
+            wiki_text += '<div class="wiki-overview">' +
+                '<ul>' +
+                "<li>" + curinfo.overview.join("</li><li>") +
+                "</li></ul></div>";
+        }
+
+        // Example information from object:
+        if (curinfo.examples.length > 0) {
+            wiki_text += '<div class="wiki-examples"><p>Beispiele: </p><ul>' +
+                "<li>" + curinfo.examples.join("</li><li>") +
+                "</li></ul></div>";
+        }
+
+        // Main text from object:
+        if (curinfo.maintext.length > 0) {
+            wiki_text += '<div class="wiki-text">' +
+                "<p>" + curinfo.maintext.join("</p><p>") +
+                '</div>'
+        }
+
+        wiki_text += "</div>";
+    }
+
+    $("#entry-list").html(wiki_text);
+
+})
---
+++ www/scripts/info_data.js
@@ -0,0 +1,28 @@
+/**
+ * Dictionary with information to be presented in the Wiki and summaries to be used in otherplaces.
+ IDs for wiki will be pre-pendend with "wiki-"
+ * @type {{}}
+ */
+
+const info_data = {
+    // Percentages:
+    "prozent": {
+        "heading": "Prozentzahlen",
+        "annotation": "",
+        "overview": ["Bezugsgröße ist entscheidend, besonders für <a href=\"#wiki-rel\">relative Angaben</a>" +
+        "(z.B., eine Erhöhung von 50%, eine Impfstoffwirksamkeit von 90%)",
+            "Absolute Ereigniswahrscheinlichkeiten sind <a href=\"#wiki-rel\">relativen Angaben</a> vorzuziehen"],
+        "examples": [],
+        "maintext": ["Prozentzahlen sollten nur bei Angaben größer als 1% verwendet werden.",
+            "Besser als <a href=\"\">Prozentzahlen</a> sind <a href=\"#wiki-nh\">natürliche Häufigkeiten</a>."]
+    },
+    "nh": {
+        "heading": "Natürliche Häufigkeiten",
+        "annotation": "[TODO: Improve design of examples!]<br>",
+        "overview": ["Bezugsgröße sollte konstant sein.",
+            "Absolute Ereigniswahrscheinlichkeiten sind relativen Angaben vorzuziehen"],
+        "examples": ["440 von 1000 Personen ohne Auffrischungsimpfung gegen COVID-19 erkranken, während unter den " +
+        "Geimpften nur 270 von 1000 erkranken.<br><a href=\"https://www.hardingcenter.de/de/impfungen/mrna-schutzimpfung-gegen-covid-19-fuer-erwachsene-unter-60-jahren\">https://www.hardingcenter.de/de/impfungen/mrna-schutzimpfung-gegen-covid-19-fuer-erwachsene-unter-60-jahren</a>"],
+        "maintext": []
+    }
+}
